538bda84f47cf0bfd24fded728b01b5a3bcd0b9d
hugorbd <hugoo.rodriguess212@gmail.com>
2025-05-18 15:33:36 -0300

    chore: adiciona conexao.py ao .gitignore

--- conexao.py
+++
@@ -1,3 +0,0 @@
-username = "ADMIN"
-password = "Projetoaviao.3"
-connect_string = "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-saopaulo-1.oraclecloud.com))(connect_data=(service_name=g80d8726eee30b8_avf0yzv2lqv1v0jx_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
